c90778a418d6f63d77ddd015713603a538859bb8
Andrew Norton <hskrus@gmail.com>
2017-02-28 22:39:51 -0500

    Barebones echo bot

---
+++ app.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import json
+
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def webhook():
+  data = request.get_json()
+
+  # We don't want to reply to ourselves!
+  if data['name'] != 'apnorton-test-bot':
+    msg = '{}, you sent "{}".'.format(data['name'], data['text'])
+    send_message(msg)
+
+  return "ok", 200
+
+def send_message(msg):
+  url  = 'https://api.groupme.com/v3/bots/post'
+
+  data = {
+          'bot_id' : os.getenv('GROUPME_BOT_ID'),
+          'text'   : msg,
+         }
+  request = Request(url, urlencode(data).encode())
+  json = urlopen(request).read().decode()
+  
+def log(msg):
+  print(str(msg))
+  sys.stdout.flush()

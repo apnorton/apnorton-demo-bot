3c6748160f24face096419e115369680dfd65792
Dave <contact@davidwhitcome.com>
2018-09-13 20:15:42 -0500

    Added delay for bot sending msg

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import json
+import time
 
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
@@ -23,6 +24,7 @@ def webhook():
   return "ok", 200
 
 def send_message(msg):
+  time.sleep(1) # prevent reply from showing before msg that bot is answering
   url  = 'https://api.groupme.com/v3/bots/post'
   data = {
           'bot_id' : os.getenv('GROUPME_BOT_ID'),
